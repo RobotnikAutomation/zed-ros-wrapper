cc67f93f53782c9205ab08df49a025688cf1e91f
Robert Vasquez Zavaleta <rvasquez@robotnik.es>
2022-03-22 12:15:34 +0100

    zed_bringup: Add zed bringup and improve autorun script

--- zed_bringup/scripts/bringup.sh
+++ zed_bringup/scripts/bringup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Network configuration
 export ROBOT_HOSTNAME=192.168.0.200
 export JETSON_HOSTNAME=192.168.0.50
 export JETSON_WORKSPACE=~/catkin_ws
 
+# Camera 1 configuration
+export JETSON_CAMERA_1_MODEL=zed2
+export JETSON_CAMERA_1_ID=front_rgbd_camera
+export JETSON_CAMERA_1_DEVICE_ID=0
+
+# Camera 2 configuration
+export JETSON_CAMERA_2_MODEL=none
+export JETSON_CAMERA_2_ID=rear_rgbd_camera
+export JETSON_CAMERA_2_DEVICE_ID=1
+
 export ROS_MASTER_URI=http://$ROBOT_HOSTNAME:11311
 export ROS_IP=$JETSON_HOSTNAME
-# export ROS_HOSTNAME=$HOSTNAME
-
 source /opt/ros/melodic/setup.bash
 source $JETSON_WORKSPACE/devel/setup.bash
 
@@ -19,4 +28,4 @@ echo ""
 
 killall screen
 sleep 2;
-screen -S zed -d -m roslaunch zed_wrapper zed.launch --wait;
+screen -S zed -d -m roslaunch zed_bringup zed_complete.launch --wait;
