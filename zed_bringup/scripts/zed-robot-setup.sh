00e704dbff51e6ebf5844cccabb2fe9580b4f71b
Robert Vasquez Zavaleta <rvasquez@robotnik.es>
2022-03-29 11:57:38 +0200

    zed_bringup: Add automatic robot setup script

---
+++ zed_bringup/scripts/zed-robot-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting installation..."
+
+echo "Install chrony"
+sudo cp ntp/chrony.conf /etc/chrony
+sudo apt-get install -y chrony
+sudo systemctl daemon-reload
+sudo systemctl enable chrony
+sudo systemctl stop chrony
+sudo systemctl start chrony
+
+echo "Installation completed"
+
+
